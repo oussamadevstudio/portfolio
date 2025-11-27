0435d0aafea76a9839fa78405c2db40253228b2a
oussamadev.studio <oussamadev.studio@gmail.com>
2025-11-27 15:11:29 +0100

    some changes to make the page responsive

---
+++ sideBar.js
@@ -0,0 +1,10 @@
+function showSideBar(){
+    const sideBar = document.querySelector(".sidebar");
+    sideBar.style.display = "flex";
+}
+
+
+function hideSideBar(){
+    const sideBar = document.querySelector(".sidebar");
+    sideBar.style.display = "none";
+}
