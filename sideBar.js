658625672398ec38c0d7cc3bc38562739de9eebb
oussamadev.studio <oussamadev.studio@gmail.com>
2025-12-01 12:16:49 +0100

    adding more responsive behaviors to the page

--- sideBar.js
+++ sideBar.js
@@ -1,10 +1,25 @@
-function showSideBar(){
+function showSideBar(event){
+
+    if(event) event.preventDefault();
+
     const sideBar = document.querySelector(".sidebar");
+    const nonOpenedTag = document.querySelector('.toggeled');
+
     sideBar.style.display = "flex";
+    nonOpenedTag.classList.add('open');
+
 }
 
 
-function hideSideBar(){
+function hideSideBar(event){
+
+    if(event) event.preventDefault();
+
     const sideBar = document.querySelector(".sidebar");
+    const nonOpenedTag = document.querySelector('.toggeled');
+
     sideBar.style.display = "none";
+    nonOpenedTag.classList.remove('open');
+    
 }
+
